aea9ecc41fd7bfa49ec211fd888cabe70c11f19d
Joanna Itzel Perez Ordaz <240222@utxicotepec.edu.mx>
2026-03-27 14:35:10 -0600

    add: se agregaron detalles

--- controller/controller_categoria.js
+++ controller/controller_categoria.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-const categoria = require('../models').tbc_categorias;
+const db = require('../models');
+const categoria = db.tbc_categorias;
 
 module.exports = {
     create(req, res){
@@ -11,7 +12,7 @@ module.exports = {
         .catch(error => res.status(400).send(error));
     },
     list(_, res){
-        return categoria.findAll()
+        return categoria.findAll({})
         .then(categorias => res.status(200).send(categorias))
         .catch(error => res.status(400).send(error));
     },
@@ -20,7 +21,7 @@ module.exports = {
         const nombre = req.params.nombre || req.query.nombre;
 
         if (id) {
-            return categoria.findByPk(id)
+            return categoria.findAll(id)
             .then(categoriaItem => {
                 if (!categoriaItem) {
                     return res.status(404).send({message: 'Categoria no encontrada'});
@@ -41,31 +42,23 @@ module.exports = {
         return res.status(400).send({message: 'Debe proporcionar id o nombre para buscar'});
     },
     update(req, res){
-        const id = req.params.id;
-        return categoria.findByPk(id)
-        .then(categoriaItem => {
-            if (!categoriaItem) {
-                return res.status(404).send({message: 'Categoria no encontrada'});
+        return categoria.update({
+            nombre: req.body.nombre,
+        }, {
+            where: {
+                id: req.params.id
             }
-            return categoriaItem.update({
-                nombre: req.body.nombre,
-            })
-            .then(updated => res.status(200).send(updated))
-            .catch(error => res.status(400).send(error));
         })
+        .then(() => res.status(200).send({message: 'Datos Actualizados'}))
         .catch(error => res.status(400).send(error));
     },
     delete(req, res){
-        const id = req.params.id;
-        return categoria.findByPk(id)
-        .then(categoriaItem => {
-            if (!categoriaItem) {
-                return res.status(404).send({message: 'Categoria no encontrada'});
+        return categoria.destroy({
+            where:{
+                id: req.params.id
             }
-            return categoriaItem.destroy()
-            .then(() => res.status(200).send({message: 'Categoria eliminada'}))
-            .catch(error => res.status(400).send(error));
         })
+        .then(() => res.status(200).send({message: 'Datos Eliminados'}))
         .catch(error => res.status(400).send(error));
     },
 };
